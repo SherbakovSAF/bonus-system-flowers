f9c46ad43dfb3085a662ef42eaad944b25a20cde
SherbakovSAF <sherbakovsaf@gmail.com>
2023-02-28 22:38:06 +0300

    Инициализация проекта

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,59 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <header class="banner__block">
+      <h1>Луг</h1>
+  </header>
+  <main>
+    <article class="card__client">
+        <div class="info_wrap"
+              v-for="(clientInfo, idx) in clientStorage"
+              v-bind:key="idx">
+          <h2>{{ clientInfo.name }}</h2>
+          <h1>{{ clientInfo.number }}</h1>
+          <h3>{{ clientInfo.points}}</h3>
+        </div>
+    </article>
+    <input type="text" placeholder="Введите номер гостя"
+    v-model="numberInput">
+  </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data(){
+    return{
+      numberInput: "",
+      clientStorage: [
+        {
+          number: 81234567890,
+          name: "Сергей",
+          points: 123,
+          purchaseHistory: [
+            {
+              date: "23.21.2222",
+              sum: 123,
+              typeOperationPoint: "SUB", // or "PLUS"
+              listPurchase: "Тюльпаны",
+            },
+          ]
+        }
+      ]
+    }
+  },
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+:root{
+  --main__accent__color: #305346,
+  --main__bg: #FBDCD7,
+  --main__text_color: #686767,
 }
+
+h1{
+  background-color: ;
+}
+
 </style>
