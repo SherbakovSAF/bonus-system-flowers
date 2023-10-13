2330172371dc5befd4d783b028ce5cf4d86fb6d6
SherbakovSAF <sherbakovsaf@gmail.com>
2023-10-13 21:26:29 +0300

    Настройка TypeScript для обращения this.

--- client/src/shims-vue.d.ts
+++ client/src/shims-vue.d.ts
@@ -1,6 +1,43 @@
-/* eslint-disable */
-declare module '*.vue' {
-  import type { DefineComponent } from 'vue'
-  const component: DefineComponent<{}, {}, any>
-  export default component
+import { Store } from '@/store';// path to store file
+
+declare module '@vue/runtime-core' {
+  interface ComponentCustomProperties {
+    $store: Store;
+  }
 }
+/* eslint-disable */
+// import { Store } from 'vuex'
+
+// declare module '*.vue' {
+//   import type { DefineComponent } from 'vue'
+//   const component: DefineComponent<{}, {}, any>
+//   interface State {
+//     count: number
+//   }
+//   interface ComponentCustomProperties {
+//     $store: Store<State>
+//   }
+//   export default component
+// }
+
+// import { Store } from 'vuex'
+
+// declare module '*.vue'; {
+//   // declare your own store states
+//   import type { DefineComponent } from 'vue';
+//   const component: DefineComponent<{}, {}, any>
+//   interface State {
+//     clientStorage: [],
+//     selectedClient: {},
+//     newClientNumber: "",
+//     modalInfo: {}
+//   }
+
+//   // provide typings for `this.$store`
+//   interface ComponentCustomProperties {
+//     $store: Store<State>
+//   }
+//   export default component
+// }
+
+
