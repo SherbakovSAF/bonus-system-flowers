cdbd934c27b13fbb68c3f91329a52f547c97a373
SherbakovSAF <sherbakovsaf@gmail.com>
2023-10-16 21:32:07 +0300

    Изменён вариант запроса в БД
    Запрос в БД теперь делается путём ожидания окончания ввода, а не после того, как количество символов будет равно определённому значению

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
           <div>
-               <main-loader-UI v-if="isLoader || useMaskPhone.length < countQueryClientCard"/>
+               <main-loader-UI v-if="isLoader"/>
                <div v-else>
                     <!-- if и for вместе не рекомендуется использовать -->
                     <div v-if="filterClientNumber.length">
@@ -27,21 +27,26 @@ import PhoneMask from '@/utils/phoneMask';
 
 // Типизиация
 import { defineComponent } from 'vue';
-import type {ClientInfo} from '@/interfaces'
+import type { ClientInfo } from '@/interfaces'
 
 export default defineComponent({
      name: "HomeView",
-     components: { ClientCard, CreateClientCard, MainLoaderUI, MainInputUI},
+     components: { ClientCard, CreateClientCard, MainLoaderUI, MainInputUI },
      data() {
           return {
                numberInput: '' as string,
-               isLoader: false as boolean,
+               isLoader: true as boolean,
                isClientsFound: false as boolean,
-               countQueryClientCard: 7 as number,
+               timer: null as ReturnType<typeof setTimeout> | null,
           }
      },
      methods: {
-          async executeRequest(): Promise<void>{
+          delayedStart() {
+               if(this.numberInput.length < 8) return
+               if (this.timer) clearTimeout(this.timer);
+               this.timer = setTimeout(this.executeRequest, 1000);
+          },
+          async executeRequest(): Promise<void> {
                try {
                     this.isLoader = true
                     await this.$store.dispatch('getClientStorageFromDB', this.useMaskPhone)
@@ -53,19 +58,16 @@ export default defineComponent({
           }
      },
      computed: {
-          displayBeforeRequest():boolean{
-               return this.useMaskPhone.length === this.countQueryClientCard
+          filterClientNumber(): Array<ClientInfo> {
+               return this.$store.state.clientStorage.filter((e: ClientInfo) => e.phone_number.startsWith(this.useMaskPhone))
           },
-          filterClientNumber():Array<ClientInfo> {
-               return this.$store.state.clientStorage.filter((e: ClientInfo)=> e.phone_number.startsWith(this.useMaskPhone))
-          },
-          useMaskPhone():string{
+          useMaskPhone(): string {
                return new PhoneMask(this.numberInput).forDateBase()
           },
      },
      watch: {
           numberInput() {
-               if(this.displayBeforeRequest) this.executeRequest()
+               this.delayedStart()
           }
      }
 }
