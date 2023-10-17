a0537e5f2326a1f79eb6c0066ba0837e39d528cb
SherbakovSAF <sherbakovsaf@gmail.com>
2023-10-17 19:44:47 +0300

    Престроение функциональности модального окна
    Теперь модальное окно это просто компонент, который отдаёт True или False.
    
    В замен компонент ждёт тип обращения, по дефолту alert(С кнопкой 'Да'), и сообщения для окна

--- client/src/components/ClientCard.vue
+++ client/src/components/ClientCard.vue
@@ -2,7 +2,7 @@
      <article class="bg-[#FBDCD7] w-1/2 rounded-r-full shadow-lg mt-6 flex justify-around items-center cursor-pointer
           max-[500px]:relative
           max-lg:w-2/3 max-sm:w-full"
-          @click="selectClient(cardInfo)">
+          @click="selectClient">
           <div class="text-[#686767] px-8 py-7">
                <div class="person__main__block">
                     <p class="text-xl">{{ cardInfo.name }}</p>
@@ -30,48 +30,56 @@
           </div>
           <img src="@/assets/media/flower.png" alt="Flowers"
                class=" h-[150px] max-[500px]:absolute left-full -translate-x-full">
-          <button @click.stop="deleteClientInfo(cardInfo)">
+          <button @click.stop="isModalActive = true">
                <img src="@/assets/media/clear.svg"
                     class="w-16 h-16 bg-main-green rounded-full p-1" alt="DeleteClientInfo" />
           </button>
+          
      </article>
+     <ModalWindow v-if="isModalActive" @responseModalWindow="confirmModalResponse"
+          typeModal="confirm" 
+          alertMessage="Вы действительно хотите удалить информацию о клиенте?"/>
 </template>
 
 <script lang="ts">
+// Компоненты
+import ModalWindow from './ModalWindow.vue';
+
+// Утилиты
 import Formating from '@/utils/formating'
 import PhoneMask from '@/utils/phoneMask'
 
+// Типизация TypeScript
 import { defineComponent } from 'vue';
 import type { ClientInfo } from '@/interfaces';
 
 export default defineComponent({
      name: "ClientCard",
+     components: {ModalWindow},
+     data(){
+          return {
+               isModalActive: false as boolean, 
+          }
+     },
      props: {
           cardInfo: {type: Object as ()=> ClientInfo, required: true} 
      },
      methods: {
-          selectClient(clientInfo: ClientInfo): void{
-               this.$store.commit('selectClient', clientInfo)
+          confirmModalResponse (event: boolean){
+               if(event) this.deleteClientInfo()
+               this.isModalActive = false
+          },
+          selectClient(): void{
+               this.$store.commit('selectClient', this.cardInfo.id)
+               console.log('Я сработал')
                this.$router.push({path: "profile"})
           },
-          deleteClientInfo(clientInfo: ClientInfo){
-               // this.$store.commit('activeModalInfo', {text: 'Вы уверены, что данного клиента надо удалять?', type: 'confirm'})
+          deleteClientInfo(){
                try {
-                    this.$store.dispatch('deleteClient', clientInfo.id)
+                    this.$store.dispatch('deleteClient', this.cardInfo.id)
                } catch (error) {
                     alert('Ошибка. Информация о клиенте не была удалена')
                }
-               // Не уверен в этом куске кода
-               // let checkAnswer = setInterval(()=>{
-               //      if(this.$store.getters.getModalCallback == true){
-               //           this.$store.commit('deleteClientInfo', clientInfo)
-               //           clearInterval(checkAnswer)
-               //      }
-               //      if(this.$store.getters.getModalCallback == false){
-               //           clearInterval(checkAnswer)
-               //      }
-               // }, 500)
-          
           }
      },
      computed: {
